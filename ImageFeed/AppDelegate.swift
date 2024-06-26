412e9c511f0dbac679b300ef70225f6e227b3aa1
Artem Polivanov <polivanov.a@yandex.ru>
2024-06-26 09:10:13 +0300

    Fix warnings

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         let sceneConfiguration = UISceneConfiguration(
-            name: "Main",
+            name: "Default Configuration",
             sessionRole: connectingSceneSession.role
         )
         sceneConfiguration.delegateClass = SceneDelegate.self
